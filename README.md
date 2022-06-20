0b5bc0556896c1787a627ef0ef241b305ff8494b
John <john.loverich@gmail.com>
2022-06-20 09:19:22 -0700

    Moving to poetry and preparing to use in other project

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # classification with stripe layers
+Currently an experiment.  Will be moved to high-order-layers-torch when I determine this is valuable.
+
 A high order approach to image classification.  Uses the "stripe" layers for "convolutional" and
 fully connected layers.  However, neither of these are the same as the standard version.  Instead
 of convolutional blocks, stripes across the entire domain are used along with a positional encoding.
@@ -8,10 +10,10 @@ the inputs from all of the stripes (and their rotations).
 ##
 Convolutional like
 ```
-python cifar100.py segments=10 n=5 max_epochs=10 rotations=2 periodicity=2.0
+python examples/cifar100.py segments=10 n=5 max_epochs=10 rotations=2 periodicity=2.0
 ```
 ##
 Fully connected like
 ```
-python cifar100invariant.py segments=10 n=3 max_epochs=10 rotations=1 periodicity=2.0 out_features=50
+python examples/cifar100invariant.py segments=10 n=3 max_epochs=10 rotations=1 periodicity=2.0 out_features=50
 ```
--- examples/cifar100invariant.py
+++ examples/cifar100invariant.py
@@ -19,7 +19,8 @@ from expansion import *
 from StripeLayer import *
 
 transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
 
 
 class AccuracyTopK(Metric):
@@ -30,16 +31,14 @@ class AccuracyTopK(Metric):
     def __init__(self, top_k=1, dist_sync_on_step=False):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
         self.k = top_k
-        self.add_state("correct", default=torch.tensor(
-            0.0), dist_reduce_fx="sum")
-        self.add_state("total", default=torch.tensor(
-            0.0), dist_reduce_fx="sum")
+        self.add_state("correct", default=torch.tensor(0.0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0.0), dist_reduce_fx="sum")
 
     def update(self, logits, y):
         _, pred = logits.topk(self.k, dim=1)
         pred = pred.t()
         corr = pred.eq(y.view(1, -1).expand_as(pred))
-        self.correct += corr[:self.k].sum()
+        self.correct += corr[: self.k].sum()
         self.total += y.numel()
 
     def compute(self):
@@ -60,48 +59,43 @@ class Net(LightningModule):
         segments = cfg.segments
         self._topk_metric = AccuracyTopK(top_k=5)
 
-        self.layer1 = StripePolynomial2d(n, in_channels=3, out_channels=3, width=32, height=32,
-                                         segments=segments, length=2.0, weight_magnitude=1.0, periodicity=cfg.periodicity, rotations=cfg.rotations)
-        self.layer2 = StripePolynomial2d(n, in_channels=3, out_channels=3, width=16, height=16,
-                                         segments=segments, length=2.0, weight_magnitude=1.0, periodicity=cfg.periodicity, rotations=cfg.rotations)
-        self.layer3 = StripePolynomial2d(n, in_channels=3, out_channels=3, width=8, height=8,
-                                         segments=segments, length=2.0, weight_magnitude=1.0, periodicity=cfg.periodicity, rotations=cfg.rotations)
+        self.layer1 = StripeLayer2d(
+            layer_creator=fully_connected_stripe(
+                n=n,
+                in_channels=3,
+                in_elements=1024,
+                out_features=cfg.out_features,
+                segments=cfg.segments,
+                periodicity=cfg.periodicity,
+            ),
+            height=32,
+            width=32,
+            rotations=cfg.rotations,
+        )
 
-        #self.pool = nn.MaxPool2d(2, 2)
-        self.pool = nn.AvgPool2d(2, 2)
-        self.convolution1 = torch.nn.Conv2d(
-            in_channels=3, out_channels=1, kernel_size=1)
-
-        self.fc1 = nn.Linear(192, 100)
+        self.fc1 = nn.Linear(cfg.out_features, 100)
 
     def forward(self, x):
 
         ans = self.layer1(x)
-        #print('ans1.shape', ans.shape)
-        # do some average pooling
-        ans = self.pool(ans)
-        #print('ansb.shape', ans.shape)
-        ans = self.layer2(ans)
-        #print('ans2.shape', ans.shape)
-        ans = self.pool(ans)
-        ans = self.layer3(ans)
-        #print('ans.shape', ans.shape)
-        ans = ans.flatten(start_dim=1)
-        # print('ans.shape',ans.shape)
         ans = self.fc1(ans)
 
         return ans
 
     def setup(self, stage):
-        num_train = int(self._train_fraction*40000)
+        num_train = int(self._train_fraction * 40000)
         num_val = 10000
-        num_extra = 40000-num_train
+        num_extra = 40000 - num_train
 
         train = torchvision.datasets.CIFAR100(
-            root=self._data_dir, train=True, download=True, transform=transform)
+            root=self._data_dir, train=True, download=True, transform=transform
+        )
 
         self._train_subset, self._val_subset, extra = torch.utils.data.random_split(
-            train, [num_train, 10000, num_extra], generator=torch.Generator().manual_seed(1))
+            train,
+            [num_train, 10000, num_extra],
+            generator=torch.Generator().manual_seed(1),
+        )
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -114,29 +108,37 @@ class Net(LightningModule):
         val = self._topk_metric(y_hat, y)
         val = self._topk_metric.compute()
 
-        self.log(f'train_loss', loss, prog_bar=True)
-        self.log(f'train_acc', acc, prog_bar=True)
-        self.log(f'train_acc5', val, prog_bar=True)
+        self.log(f"train_loss", loss, prog_bar=True)
+        self.log(f"train_acc", acc, prog_bar=True)
+        self.log(f"train_acc5", val, prog_bar=True)
 
         return loss
 
     def train_dataloader(self):
         trainloader = torch.utils.data.DataLoader(
-            self._train_subset, batch_size=self._batch_size, shuffle=True, num_workers=10)
+            self._train_subset,
+            batch_size=self._batch_size,
+            shuffle=True,
+            num_workers=10,
+        )
         return trainloader
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self._val_subset, batch_size=self._batch_size, shuffle=False, num_workers=10)
+        return torch.utils.data.DataLoader(
+            self._val_subset, batch_size=self._batch_size, shuffle=False, num_workers=10
+        )
 
     def test_dataloader(self):
         testset = torchvision.datasets.CIFAR100(
-            root=self._data_dir, train=False, download=True, transform=transform)
+            root=self._data_dir, train=False, download=True, transform=transform
+        )
         testloader = torch.utils.data.DataLoader(
-            testset, batch_size=4, shuffle=False, num_workers=10)
+            testset, batch_size=4, shuffle=False, num_workers=10
+        )
         return testloader
 
     def validation_step(self, batch, batch_idx):
-        return self.eval_step(batch, batch_idx, 'val')
+        return self.eval_step(batch, batch_idx, "val")
 
     def eval_step(self, batch, batch_idx, name):
         x, y = batch
@@ -149,20 +151,20 @@ class Net(LightningModule):
         val = self._topk_metric.compute()
 
         # Calling self.log will surface up scalars for you in TensorBoard
-        self.log(f'{name}_loss', loss, prog_bar=True)
-        self.log(f'{name}_acc', acc, prog_bar=True)
-        self.log(f'{name}_acc5', val, prog_bar=True)
+        self.log(f"{name}_loss", loss, prog_bar=True)
+        self.log(f"{name}_acc", acc, prog_bar=True)
+        self.log(f"{name}_acc5", val, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
         # Here we just reuse the validation_step for testing
-        return self.eval_step(batch, batch_idx, 'test')
+        return self.eval_step(batch, batch_idx, "test")
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self._lr)
 
 
-@hydra.main(config_name="./config/cifar100_config")
+@hydra.main(config_path="./config", config_name="cifar100invariant_config")
 def run_cifar100(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
     print("Working directory : {}".format(os.getcwd()))
@@ -171,9 +173,9 @@ def run_cifar100(cfg: DictConfig):
     trainer = Trainer(max_epochs=cfg.max_epochs, gpus=cfg.gpus)
     model = Net(cfg)
     trainer.fit(model)
-    print('testing')
+    print("testing")
     trainer.test(model)
-    print('finished testing')
+    print("finished testing")
 
 
 if __name__ == "__main__":
